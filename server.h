9364195d092534fefb0621860bca37550fe0fe44
burnfx <burni.stylz@gmail.com>
2014-12-16 12:57:07 +0100

    gui2

--- server.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLIENT_H_
-#define CLIENT_H_
-    extern int client;
-    void sendR(char** cmd);
-    void cleanup(int signum);
-    void startClient();
-    void stopClient(int signum);
-#endif
